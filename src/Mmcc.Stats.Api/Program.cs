33d95c1086a929fdfe706f6b39851bb5de86d277
Trace <lukaszdabrowski881@gmail.com>
2020-07-16 19:34:06 +0200

    set up project structure

---
+++ src/Mmcc.Stats.Api/Program.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Mmcc.Stats.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+        
+        public static Logger CreateLogger()
+            => new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    Path.Combine("logs", "log.txt"),
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 2
+                )
+                .CreateLogger();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/Mmcc.Stats.DataModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mmcc.Stats.DataModels
+{
+    public class Class1
+    {
+    }
+}
