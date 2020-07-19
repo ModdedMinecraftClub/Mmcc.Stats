7d126f4a3df12e49012597244799cba5a93de9e6
Trace <lukaszdabrowski881@gmail.com>
2020-07-19 07:49:40 +0200

    add user input handling

--- src/Mmcc.Stats/wwwroot/js/default.js
+++ src/Mmcc.Stats/wwwroot/js/default.js
@@ -1,41 +1,60 @@
 ﻿async function onSubmit() {
+    let fromDate = document.getElementById("from").value;
+    let toDate = document.getElementById("to").value;
+    let mode = document.getElementById("mode-input").value;
     
-    console.log(`from: ${document.getElementById("from").value}`)
-    console.log(`to: ${document.getElementById("to").value}`)
-    console.log(`mode: ${document.getElementById("mode-input").value}`)
+    if (fromDate === '' || toDate === '') {
+        alert("Date cannot be empty");
+        return;
+    }
     
-    let f = await getRaw();
-    
-    console.log(f);
-        
-    let data = createTraces(f);
-    
-    console.log(data);
+    let apiResponse = await getData(fromDate, toDate);
 
-    Plotly.newPlot('plot', data);
+    if (mode === 'Smoothed data') {
+        createPlot(apiResponse, true);
+    } else {
+        createPlot(apiResponse, false);
+    }
 }
 
-async function getRaw() {
-    let response = await fetch('https://localhost:5001/api/playerbase-stats?from=2019-12-23&to=2020-01-03');
+async function getData(fromDate, toDate) {
+    //https://localhost:5001/api/playerbase-stats?from=2019-12-23&to=2020-01-03
+    let response = await fetch(`https://localhost:5001/api/playerbase-stats?from=${fromDate}&to=${toDate}`);
     return await response.json();
 }
 
-function createTraces(data) {
+function createPlot(data, isSmooth) {
     let traces = [];
     
     for (const serverData of data) {
         if (serverData.timesList.length !== 0) {
-            let trace = {
-                name: serverData.serverName,
-                x: serverData.timesList,
-                y: smooth(serverData.playersOnlineList, 15),
-                mode: 'lines',
-                type: 'scatter'
+            if (isSmooth) {
+                traces.push(createSmoothTrace(serverData));
+            } else {
+                traces.push(createRawTrace(serverData));
             }
-            
-            traces.push(trace);
         }
     }
-    
-    return traces;
+
+    Plotly.newPlot('plot', traces);
+}
+
+function createRawTrace(serverData) {
+    return {
+        name: serverData.serverName,
+        x: serverData.timesList,
+        y: serverData.playersOnlineList,
+        mode: 'lines',
+        type: 'scatter'
+    };
+}
+
+function createSmoothTrace(serverData) {
+    return {
+        name: serverData.serverName,
+        x: serverData.timesList,
+        y: smooth(serverData.playersOnlineList, 15),
+        mode: 'lines',
+        type: 'scatter'
+    };
 }
