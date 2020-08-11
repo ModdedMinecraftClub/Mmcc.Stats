93eb5ce5835f38c099553f5d4ebfe9584fe24321
Trace <lukaszdabrowski881@gmail.com>
2020-08-11 23:33:39 +0200

    port token validation to EFCore

--- src/Mmcc.Stats.Infrastructure/Authentication/ClientAppAuthentication/ClientAppAuthentication.cs
+++ src/Mmcc.Stats.Infrastructure/Authentication/ClientAppAuthentication/ClientAppAuthentication.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Mmcc.Stats.Core.Interfaces;
+using Mmcc.Stats.Core.Data;
 
-namespace Mmcc.Stats.Infrastructure.Authentication
+namespace Mmcc.Stats.Infrastructure.Authentication.ClientAppAuthentication
 {
     public class ClientAppAuthenticationOptions : AuthenticationSchemeOptions
     {
@@ -16,18 +19,18 @@ namespace Mmcc.Stats.Infrastructure.Authentication
     
     public class ClientAppAuthenticationHandler : AuthenticationHandler<ClientAppAuthenticationOptions>
     {
-        private readonly ITokenService _tokenService;
+        private readonly IMediator _mediator;
 
         public ClientAppAuthenticationHandler(
             IOptionsMonitor<ClientAppAuthenticationOptions> options,
             ILoggerFactory logger,
             UrlEncoder encoder,
             ISystemClock clock,
-            ITokenService tokenService
+            IMediator mediator
             ) 
             : base(options, logger, encoder, clock)
         {
-            _tokenService = tokenService;
+            _mediator = mediator;
         }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -57,17 +60,12 @@ namespace Mmcc.Stats.Infrastructure.Authentication
 
         private async Task<AuthenticateResult> ValidateToken(string token)
         {
-            var validatedToken = await _tokenService.GetToken(token);
+            var validatedToken = (await _mediator.Send(new ValidateTokenViaDb.Query {IncomingTokenValue = token}))
+                .ValidatedToken;
             
-            if (validatedToken == null)
-            {
-                return AuthenticateResult.Fail("Unauthorized");
-            }
+            if (validatedToken == null) return AuthenticateResult.Fail("Unauthorized");
             
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, validatedToken.ClientName),
-            };
+            var claims = new List<Claim> {new Claim(ClaimTypes.Name, validatedToken.ClientName)};
             var identity = new ClaimsIdentity(claims, Scheme.Name);
             var principal = new System.Security.Principal.GenericPrincipal(identity, null);
             var ticket = new AuthenticationTicket(principal, Scheme.Name);
