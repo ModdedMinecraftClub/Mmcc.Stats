deebcbf1968e024b0ae4af7dbfb6119eacb87ec4
Trace <lukaszdabrowski881@gmail.com>
2020-08-12 04:58:08 +0200

    add todo

--- src/Mmcc.Stats/Features/Playerbase/PlayerbaseController.cs
+++ src/Mmcc.Stats/Features/Playerbase/PlayerbaseController.cs
@@ -2,6 +2,7 @@
 {
     public class PlayerbaseController
     {
-        
+        // TODO: GET request that returns playerbase stats for a single server
+        // TODO: GET request that returns playerbase stats for all servers
     }
 }
