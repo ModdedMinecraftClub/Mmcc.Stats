1091731515f0c94a0f0392d254fcdcd2334634a9
Trace <lukaszdabrowski881@gmail.com>
2020-08-17 06:01:47 +0100

    update publish script

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,35 @@
 param(
-    [Parameter(Mandatory=$true)][string]$platform
+    [Parameter(Mandatory=$true)][string]$dotnetRuntimeIdentifier
 )
 
-dotnet publish ./src/Mmcc.Stats -r $platform -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesInSingleFile=true --output ./out
+$starter_location = Get-Location
+
+try {
+    Set-Location ./src/Mmcc.Stats.Frontend
+
+    Write-Host "`nRestoring npm packages...`n"
+
+    npm install
+
+    if($LASTEXITCODE -ne 0) {
+        Write-Host "`nError while restoring npm packages. Exiting...`n"
+        exit
+    }
+
+    Write-Host "`nApplying security fixes...`n"
+
+    npm audit fix
+
+    if($LASTEXITCODE -ne 0) {
+        Write-Host "`nError while applying fixes. Exiting...`n"
+        exit
+    }
+}
+finally {
+    Set-Location $starter_location
+}
+
+Write-Host "`nRestored npm packages.`n"
+Write-Host "Building the app...`n"
+
+dotnet publish ./src/Mmcc.Stats -r $dotnetRuntimeIdentifier -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesInSingleFile=true --output ./out
