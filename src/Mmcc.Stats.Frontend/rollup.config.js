10126a9c36ee4b06fbc5165037003e20ed3279a1
Trace <lukaszdabrowski881@gmail.com>
2021-02-10 07:27:03 +0000

    remove old frontend

--- src/Mmcc.Stats.Frontend/rollup.config.js
+++ src/Mmcc.Stats.Frontend/rollup.config.js
@@ -1,14 +1,17 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
-	
+
 	function toExit() {
 		if (server) server.kill(0);
 	}
@@ -28,7 +31,7 @@ function serve() {
 }
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
 		format: 'iife',
@@ -37,14 +40,15 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
+			preprocess: sveltePreprocess({ sourceMap: !production }),
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
 			}
 		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -56,6 +60,10 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
--- src/Mmcc.Stats.Frontend/src/App.svelte
+++ src/Mmcc.Stats.Frontend/src/App.svelte
@@ -1,26 +1,30 @@
-<script>
-	import Navbar from "./Navbar.svelte"
-	import Footer from "./Footer.svelte"
-	import Stats from "./Stats.svelte"
+<script lang="ts">
+	export let name: string;
 </script>
 
-<div class="wrapper">
-	<div class="cont">
-		<Navbar/>
-		<Stats/>
-	</div>
-	<Footer/>
-</div>
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
 
 <style>
-	.wrapper {
-		display: flex;
-		flex-direction: column;
-		min-height: 100%;
-		min-width: 100%;
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
 	}
 
-	.cont {
-		flex-grow: 1;
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
 	}
 </style>
