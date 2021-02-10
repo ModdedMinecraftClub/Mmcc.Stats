5b15496a3ccd8cf26c452309f6de465eabbc254d
Trace <lukaszdabrowski881@gmail.com>
2021-02-10 20:56:56 +0000

    whitelist dynamic classes from purging

--- src/Mmcc.Stats.Frontend/postcss.config.js
+++ src/Mmcc.Stats.Frontend/postcss.config.js
@@ -1,5 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: ['./src/**/*.svelte', './src/**/*.html'],
+    safelist: ["bg-green-500", "bg-green-300", "bg-pink-500", "bg-pink-300", "bg-blue-500", "bg-blue-300"],
     whitelistPatterns: [/svelte-/],
     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
