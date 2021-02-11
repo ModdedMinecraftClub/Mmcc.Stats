3fe6641efafee0f8be7fc57d5cfa1716b514c771
Trace <lukaszdabrowski881@gmail.com>
2021-02-11 17:17:42 +0000

    add error handling

---
+++ src/Mmcc.Stats.Frontend/src/ApiError.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { fade } from "svelte/transition";
+</script>
+
+<p
+    class="text-center mt-2"
+    in:fade={{ delay: 400, duration: 300 }}
+    out:fade={{ duration: 300 }}
+    wmode="transparent"
+>
+    API error.
+</p>
--- src/Mmcc.Stats.Frontend/src/dashboard/DashboardMain.svelte
+++ src/Mmcc.Stats.Frontend/src/dashboard/DashboardMain.svelte
@@ -11,6 +11,7 @@
     import WeeklyTrendsCard from "./WeeklyTrendsCard.svelte";
     import { fade } from "svelte/transition";
     import TpsChartCard from "./TpsChartCard.svelte";
+import ApiError from "../ApiError.svelte";
 
     interface IBasicInfo {
         tpsAvg: TpsWeeklyAvgs;
@@ -21,7 +22,7 @@
     const pingsClient: PingsClient = new PingsClient(apiConfig);
 
     async function loadBasic(): Promise<IBasicInfo> {
-        let tpsRes: TpsWeeklyAvgs = (await tpsClient.getWeeklyAvgs());
+        let tpsRes: TpsWeeklyAvgs = await tpsClient.getWeeklyAvgs();
         let pingsRes: PingsWeeklyAvgs = await pingsClient.getWeeklyAvgs();
 
         return {
@@ -33,26 +34,45 @@
 
 <div class="bg-gray-900 text-gray-100 py-16 px-6 mb-12 lg:px-10">
     {#await loadBasic()}
-        <div in:fade={{delay: 400, duration: 300}} out:fade={{duration: 300}} wmode="transparent">
+        <div
+            in:fade={{ delay: 400, duration: 300 }}
+            out:fade={{ duration: 300 }}
+            wmode="transparent"
+        >
             <Loading />
         </div>
     {:then res}
-        <div in:fade={{delay: 400, duration: 300}} out:fade={{duration: 300}} wmode="transparent">
+        <div
+            in:fade={{ delay: 400, duration: 300 }}
+            out:fade={{ duration: 300 }}
+            wmode="transparent"
+        >
             <div id="weekly-trends">
-                <h2 class="my-2 text-lg ml-1 text-gray-300 uppercase tracking-wide">
+                <h2
+                    class="my-2 text-lg ml-1 text-gray-300 uppercase tracking-wide"
+                >
                     Weekly trends
                 </h2>
-                <WeeklyTrendsCard pingsWeeklyAvgs={res.pingsAvgs} tpsWeeklyAvgs={res.tpsAvg} />
+                <WeeklyTrendsCard
+                    pingsWeeklyAvgs={res.pingsAvgs}
+                    tpsWeeklyAvgs={res.tpsAvg}
+                />
             </div>
             <div id="charts">
-                <h2 class="mt-12 mb-2 text-lg ml-1 text-gray-300 uppercase tracking-wide">
+                <h2
+                    class="mt-12 mb-2 text-lg ml-1 text-gray-300 uppercase tracking-wide"
+                >
                     Charts
                 </h2>
-                <div class="flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:gap-x-8">
-                    <PlayerbaseChartCard pingsClient={pingsClient} />
-                    <TpsChartCard tpsClient={tpsClient} />
+                <div
+                    class="flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:gap-x-8"
+                >
+                    <PlayerbaseChartCard {pingsClient} />
+                    <TpsChartCard {tpsClient} />
                 </div>
             </div>
         </div>
+    {:catch}
+        <ApiError />
     {/await}
 </div>
